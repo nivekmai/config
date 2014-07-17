6c3a6df45382f232def101577d0c8fd5ed6aeb59
Kevin Christensen <kevin@whatsapp.com>
2014-07-16 18:44:14 -0700

    Initial commit  Initial working configuration, put, get, delete with input sanitization.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-config
+WA Config
 ======
+
+#Install
+
+1. Instal virtualenv
+  ```sudo easy_install venv```
+2. cd to src dir
+  ```cd confing/src```
+3. Activate virtualenv
+  ```. venv/bin/activate```
+4. Install Flask
+  ```pip install flask```
+5. Install Flask Restufl
+  ```pip install flask-restful```
---
+++ src/config.py
@@ -0,0 +1,70 @@
+import os
+import json
+from flask import Flask, request
+from flask.ext.restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Config(Resource):
+    def get(self, config_path):
+        self.sanitize_path(config_path)
+        if not os.path.exists(self.path):
+            return {
+                'fail': 'file does not exist',
+                'file': self.path
+            }
+        with open(self.path, 'r') as f:
+            self.json = json.load(f)
+        return self.json
+
+    def put(self, config_path):
+        self.json = str(request.form['json'])
+        self.sanitize_path(config_path)
+        #validate json
+        if not self.is_valid():
+            return {
+              'fail': 'invalid json file',
+              'data': self.json
+            }
+        #create path
+        path_dir = os.path.dirname(self.path)
+        if not os.path.exists(path_dir) and path_dir:
+            os.makedirs(path_dir)
+        #put the json there
+        with open(self.path, 'w') as f:
+            f.write(self.json)
+        with open(self.path, 'r') as f:
+            self.json = json.load(f)
+        return self.json
+
+    def delete(self, config_path):
+        self.sanitize_path(config_path)
+        try:
+            os.remove(self.path)
+            try:
+                os.rmdir(os.path.dirname(self.path))
+            except:
+                return {'success': 'file removed, but path still has content'}
+            return {'success': 'file and path removed'}
+        except:
+            return {'fail': 'file does not exist'}
+
+    # Checks to see if self.json is a valid json
+    def is_valid(self):
+        try:
+            json_object = json.loads(self.json)
+        except ValueError, e:
+            return False
+        return True
+
+     # Sanitizes a path, expects a string path, sets the objects path
+    def sanitize_path(self, config_path):
+        self.path = os.path.join('..',os.path.normpath('/'+config_path).lstrip('/'))
+
+
+api.add_resource(Config, '/<path:config_path>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
